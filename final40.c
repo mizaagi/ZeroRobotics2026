950a46cef7fb192f2f42830b3ac4d7d11564d059
Marlon Kohn <126592834+mizaagi@users.noreply.github.com>
2026-01-29 11:18:08 -0600

    Implement wCropFinal40 for crop management
    Added wCropFinal40 function to handle crop planting and watering.

--- final40.c
+++ final40.c
@@ -1,3 +1,18 @@
+void wCropFinal40(int plotNum, int cropNum) {
+	// Function to move to a plot, plant a specific crop, and set time data for the crop
+    game.MovePlot(plotNum);
+    game.PlantCrop(plotNum, cropNum);
+    waterTimes[plotNum-1] = game.GetTime();
+    charge[plotNum-1] = 1;
+	// Determines what the crop's plant time is and sets the PT times based on the crop number
+    PT[plotNum-1] = cropNum == 1 ? 6.0 : cropNum == 2 ? 3.0 : cropNum == 3 ? 5.0 : cropNum == 4 ? 12.0 : cropNum == 5 ? 3.0 : 9.0;
+    game.WaterCrop(plotNum);
+	// Crop value depreciation
+    cropValue[cropNum-1] = cropValue[cropNum-1] * 0.75;
+	// If the crop being planted is the bonus crop, set the bonus crop variable to zero to show that a new bonus crop can be generated
+    if (cropNum == bonus) bonus = 0;
+}
+
 // FINAL 40 CODE IS BELOW
 void helperFinal40(int pn) {
 	// helper determines if robot is able to do something with the plot
@@ -19,6 +34,8 @@ void helperFinal40(int pn) {
 
 void final40() {
     // Assume that bonus crops have been obtained
+    // id 5, 3, 1
+    cropValue[1] = cropValue[4];
     while (true) { // Runnable because at this point we are just running code until time runs out
         i = 0; // Variable to simulate for loop within while loop; traverse through list of plots;
         // After watering, i will reset to zero so the loop can continue if possible
